bae58dddd4775c0cc0930c7aa07ce7bafcdaa356
jongsik <jongsik@cloudflare.com>
2025-08-15 02:44:10 +0900

    add thinking bubble

--- src/ChatPage.tsx
+++ src/ChatPage.tsx
@@ -1,16 +1,24 @@
 import Chat, { Bubble, useMessages, type MessageProps } from "@chatui/core";
 import "@chatui/core/dist/index.css";
 import MarkDownToJSX from "markdown-to-jsx";
+import { useState } from "react";
 
 export function ChatPage() {
-  const { messages, appendMsg } = useMessages([]);
+  const { messages, appendMsg, updateMsg } = useMessages([]);
+  const [isTyping, setIsTyping] = useState(false);
 
   async function handleSend(type: string, val: string) {
     if (type === "text" && val.trim()) {
+      setIsTyping(true);
       appendMsg({
         type: "text",
         content: { text: val },
         position: "right",
+      });
+      const id = appendMsg({
+        type: "text",
+        content: { text: "thinking..." },
+        position: "left",
       });
       const res = await fetch("/chat", {
         method: "POST",
@@ -22,7 +30,7 @@ export function ChatPage() {
 
       if (!res.ok) {
         const error = (await res.json()) as { message: string };
-        appendMsg({
+        updateMsg(id, {
           type: "text",
           content: { text: error.message },
           position: "left",
@@ -31,11 +39,13 @@ export function ChatPage() {
       }
 
       const data = (await res.json()) as returnMSG;
-      appendMsg({
+      updateMsg(id, {
         type: "text",
         content: { text: data.output[1].content[0].text },
         position: "left",
       });
+
+      setIsTyping(false);
     }
   }
 
@@ -52,9 +62,9 @@ export function ChatPage() {
     <Chat
       locale="en-US"
       placeholder="please enter.."
-      isTyping={true}
       navbar={{ title: "Workers AI Chat" }}
       messages={messages}
+      isTyping={isTyping}
       renderMessageContent={renderMessageContent}
       onSend={handleSend}
     />
